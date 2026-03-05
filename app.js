bbb051bea6265810ea4ef3bb3c4b49c79562a76f
varshinigowda0731-hash <varshinigowda0731@gmail.com>
2026-03-05 07:37:09 +0530

    Update response message for root route

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello! this is varshini's devops project 🚀");
+  res.send("CI Test successful - varshini's devops project 🚀");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
