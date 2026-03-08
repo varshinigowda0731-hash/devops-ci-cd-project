82804dc1eeee17bb1bf8f99ece9e9a5a62d1af62
varshinigowda0731-hash <varshinigowda0731@gmail.com>
2026-03-08 13:17:57 +0530

    Change response message for root route
    Updated the response message for the root route to reflect the DevOps pipeline status.

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI test successful - varshini's devops project 🚀");
+  res.send("Devops pipeline working perfectly - varshini 🚀");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
